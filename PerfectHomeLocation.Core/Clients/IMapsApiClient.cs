5d5fe765627771a35e9c6525513cf538f80ee35b
Aaron Schendel <aaron.schendel@gmail.com>
2022-12-29 01:02:04 -0600

    removing .core project for now

--- PerfectHomeLocation.Core/Clients/IMapsApiClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PerfectHomeLocation.Core.Clients
-{
-    public interface IMapsApiClient
-    {
-        Task<SearchByTextResponse> SearchForPlace(string searchString);
-        Task<PlaceDetailsResponse> GetPlaceDetailsResponse(string placeId);
-    }
-}
-
